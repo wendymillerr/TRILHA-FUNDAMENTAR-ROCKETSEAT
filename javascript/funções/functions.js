1085af7d8e43032894d168ce8b06745a735a74b6
Wendy Miller <weendymilleer@gmail.com>
2023-01-04 10:27:04 -0300

    parametros

--- javascript/funções/functions.js
+++ javascript/funções/functions.js
@@ -7,4 +7,10 @@ function createPhrases(){
 }
 
 createPhrases()
+
+const sum = function( number1, number2){
+    return total = number1 + number2
+}
+console.log(sum(2, 3))
+console.log(total) // funciona com o var, e sem nome de const e let. mas nao faça, pode dar erro de referencia, entao sempre declare como variavel local
 console.log('fim do programa')
