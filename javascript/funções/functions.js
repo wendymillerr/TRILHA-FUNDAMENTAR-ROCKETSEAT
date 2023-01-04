b596932fd62142459442a728a77a9a34333f4f60
Wendy Miller <weendymilleer@gmail.com>
2023-01-04 10:18:57 -0300

    function

---
+++ javascript/funções/functions.js
@@ -0,0 +1,10 @@
+
+
+function createPhrases(){
+    console.log('Estudar é muito bom')
+    console.log('Paciencia e persistencia')
+    console.log('Revisão é a mãe do apredizado')
+}
+
+createPhrases()
+console.log('fim do programa')
