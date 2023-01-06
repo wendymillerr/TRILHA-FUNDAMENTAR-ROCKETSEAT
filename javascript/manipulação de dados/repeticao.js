b44f547011f27d96e732ee952361cd2b44e6a5df
Wendy Miller <weendymilleer@gmail.com>
2023-01-06 11:43:55 -0300

    estruturas de repetição

--- javascript/manipulação de dados/repeticao.js
+++ javascript/manipulação de dados/repeticao.js
@@ -6,3 +6,24 @@ while(i < 10){
     i++;
     console.log(i)
 }
+
+let name = 'Mayk'
+let names = ['joao', 'paulo', 'pedro']
+
+for(let char of name){
+    console.log(char)
+}
+
+for (let name of names){
+    console.log(name)
+}
+
+let person = {
+    name:john,
+    age: 30, 
+    weight: 86.7
+}
+
+for (let property in person){
+    console.log(person[property])
+}
