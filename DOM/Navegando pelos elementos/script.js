1ad71c75d8787ef930775390c31a9a18946bbba1
Wendy Miller <weendymilleer@gmail.com>
2023-01-13 11:13:45 -0300

    parents

---
+++ DOM/Navegando pelos elementos/script.js
@@ -0,0 +1,9 @@
+const element = document.querySelector('body')
+console.log(element.parentNode)
+const el = document.querySelector('body')
+console.log(el.childNodes)
+//firstchild leva em conta espaços vazios
+//firstelementchild
+//lastchild 
+//lastElementChild
+//children vem com html colection
