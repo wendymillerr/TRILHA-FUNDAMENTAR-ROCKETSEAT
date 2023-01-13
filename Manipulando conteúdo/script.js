124a15384b46a17a9320917df875ea620f22aa7d
Wendy Miller <weendymilleer@gmail.com>
2023-01-13 09:05:52 -0300

    manipulando conteudo

---
+++ Manipulando conteúdo/script.js
@@ -0,0 +1,14 @@
+const element = document.querySelector("h1")
+
+element.textContent += " olá gente!"
+
+console.log(element.textContent)
+
+const element2 = document.querySelector("h2")
+
+element2.innerText = "bom dia devs"
+
+const element3 = document.querySelector("h3")
+
+
+element3.innerHTML = "bom dia devs <small>!!!</small>"
