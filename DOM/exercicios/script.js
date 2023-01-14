0ec38aeec71c03035dc48411bd29496e280497bb
Wendy Miller <weendymilleer@gmail.com>
2023-01-14 15:30:59 -0300

    exercicio dom

--- DOM/exercicios/script.js
+++ DOM/exercicios/script.js
@@ -1,4 +1,14 @@
+const button =  document.getElementById('openModal');
+const modalWrapper = document.querySelector('.modal-wrapper');
 
-function invisible(){
-
+button.onclick = function(){
+    modalWrapper.classList.remove('invisible');
 }
+//procure dps verificar se a classe invisible ja existe pq ta em loop
+document.addEventListener('keydown', function(event){
+    const isEscKey = event.key == 'Escape';
+
+    if(isEscKey){
+        modalWrapper.classList.add('invisible') //loop
+    }
+})
