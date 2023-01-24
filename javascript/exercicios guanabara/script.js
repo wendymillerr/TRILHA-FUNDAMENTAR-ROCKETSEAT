8bf3b1325ce3040e7a1e1c0117a5adcb7c900f8c
Wendy Miller <weendymilleer@gmail.com>
2023-01-24 11:43:34 -0300

    função funciona

--- javascript/exercicios guanabara/script.js
+++ javascript/exercicios guanabara/script.js
@@ -1,9 +1,10 @@
 function verificar(){
+    
     var data = new Date()
     var ano_atual = data.getFullYear()
     var fano = document.getElementById('ano_nascimento')
     var res = document.getElementById('res')
-    if(fano.ariaValueMax.length == 0 || Number(fano.value) > ano_atual){
+    if(fano.value.length == 0 || Number(fano.value) > ano_atual){
         window.alert('[ERRO] verifique os dados e tente novamente!')
     }else{
         var fsex = document.getElementsByClassName('sex')
@@ -15,7 +16,8 @@ function verificar(){
         }else{
             genero = 'Homem'
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
     }
-
+    
 }
