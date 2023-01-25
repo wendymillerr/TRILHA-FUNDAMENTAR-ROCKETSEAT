ed501aec6164c3b07bc95892f004418dae59e3ff
Wendy Miller <weendymilleer@gmail.com>
2023-01-25 07:49:40 -0300

    adicionar foto por meio da DOM

---
+++ javascript/exercicios guanabara/add foto do js para html/custom.js
@@ -0,0 +1,7 @@
+var img = document.createElement('img')
+img.setAttribute('id', 'foto')
+img.setAttribute('src', 'imagem.png')
+
+var var1 = document.getElementById('add')
+img.style.width = '100px'
+var1.appendChild(img)
