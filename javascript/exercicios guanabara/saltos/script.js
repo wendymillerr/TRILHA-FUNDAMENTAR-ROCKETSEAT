b6e27f3d080089c2a54d73e17e374b9f99281e64
Wendy Miller <weendymilleer@gmail.com>
2023-01-26 10:15:44 -0300

    exercicio contagem

---
+++ javascript/exercicios guanabara/saltos/script.js
@@ -0,0 +1,20 @@
+function countJump(){
+    console.log('entrou')
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var jump = document.getElementById('passo')
+    var count = document.getElementById('count')
+    for(var i = 0; i < fim.value; i++){
+        var result = 0;
+        result = inicio + jump;
+        if(inicio == fim || inicio > fim){
+            window.alert('Erro! dados inválidos')
+        }
+        if(result > fim.value){
+            return 
+        }
+        console.log('entrou aqui')
+        count.innerHTML = 'acabou'
+        
+    }
+}
