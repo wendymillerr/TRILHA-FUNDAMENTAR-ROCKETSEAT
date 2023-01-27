08f678bb05b6dc9b5fb92697522e31986617f584
Wendy Miller <weendymilleer@gmail.com>
2023-01-26 22:02:17 -0300

    execicio completo

--- javascript/exercicios guanabara/saltos/script.js
+++ javascript/exercicios guanabara/saltos/script.js
@@ -2,19 +2,19 @@ function contar(){
    
     let inicio = document.getElementById('inicio')
     let fim = document.getElementById('fim')
-    let jump = document.getElementById('passo')
+    let jump = document.getElementById('jump')
     let count = document.getElementById('count')
     //count.innerHTML = 'troquei'
    if(inicio.value.length == 0 || fim.value.length == 0 || jump.value.length == 0){
     window.alert('[ERRO] faltam dados!')
    }else {
-    count.innerHTML = 'Contando...'
+    count.innerHTML = 'Contando: '
     let i = Number(inicio.value)
     let f = Number(fim.value)
     let j = Number(jump.value)
 
-    for(let c = i; c <= f; c += p ){
-        count.innerHTML = `${c} \u{1F603}`
+    for(let c = i; c <= f; c += j ){
+        count.innerHTML += `${c} \u{1F603}`
     }
 
    }
