5e1cc7f0ab4432118cedd482d40172603a634353
Wendy Miller <weendymilleer@gmail.com>
2023-01-31 08:38:36 -0300

    funções corretas

--- javascript/exercicios guanabara/lista de números/script.js
+++ javascript/exercicios guanabara/lista de números/script.js
@@ -1,6 +1,6 @@
 const arrayOfElements = []
 const listOfNumbers = document.getElementById('listOfNumbers')
-const buttonFinalize = document.getElementById('finalize')
+const paragrafo = document.getElementById('paragrafos')
 
 
 
@@ -16,7 +16,59 @@ function inputElementInArray(){
         }
 }
 
+function soma(){
+    var soma = 0;
+    for(let i = 0; i < arrayOfElements.length; i++){
+        soma += Number(arrayOfElements[i]);
+    }
+    return soma;
+}
+
+function maxElementOfArray(){
+    let max = 0;
+    for(let i = 0; i < Number(arrayOfElements.length); i++){
+        
+        if (arrayOfElements[i].value > max){
+            max = arrayOfElements[i].value;
+        }
+    }
+ 
+    return max;
+}
+
+function minElementOfArray(){
+    min = arrayOfElements[0];
+    for(let i = 0; i < Number(arrayOfElements.length); i++){
+       if (arrayOfElements[i] < min){
+        min = arrayOfElements[i];
+       }
+    }
+    return min;
+}
+
+function average(){
+
+   let somaDosElementos = soma();
+   let average = somaDosElementos / arrayOfElements.length;
+   return average;
+
+}
+
 function getArray(){
-    const p = document.createElement('p')
+ window.alert('entrou')
+ paragrafo.innerHTML = "mudou"
+ 
+    let min = minElementOfArray();
+    let somaOfElements = soma();
+    let max = Math.max.apply(null, arrayOfElements);
+    let averageElements = average();
+   
     
+    paragrafo.innerHTML = 
+    `Ao todo, temos ${arrayOfElements.length} números cadastrados. \n
+    O maior valor informado foi ${max}. \n
+    O menor valor informado foi ${min}.\n
+    Somando todos os valores, temos ${somaOfElements}\n
+    A média dos valores digitados é ${averageElements}.`
+
 }
